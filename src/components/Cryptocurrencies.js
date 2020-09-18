388ff0da9a56bf6e41877e0a9f1a7ae5bbbcf98b
Ziad <ziad1991@hotmail.co.uk>
2020-09-18 14:42:22 +0100

    List all coins onto page

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -2,16 +2,31 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const Cryptocurrencies = () => {
+    const [coins, setCoins] = useState([])
+    const [load, setLoad] = useState(false)
 
     useEffect(() => {
         axios.get('https://api.coingecko.com/api/v3/coins/list')
-            .then(res => console.log(res))
-            .catch(error => console.log(error))
-    })
-    
+            .then(res => {
+                setCoins(res.data)
+                setLoad(true)
+            })
+            .catch(err => {
+                console.log(err)
+                setLoad(true)
+            })
+    },[])
+    console.log(coins)
+
     return (
         <div className='Cryptocurrencies'>
-            Here is a list of cryptocurrencies
+            <ul>
+                { load ? (
+                    coins.map((coin, index) => <li> {coin.id} </li>) 
+                ) : (
+                    <h1>Loading...</h1>
+                )}
+            </ul>
         </div>
     )
 }
