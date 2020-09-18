ec8ba7600ed1c6752e9c7cd3b2f07031660e3111
Ziad <ziad1991@hotmail.co.uk>
2020-09-18 13:55:58 +0100

    Use axios to fetch data from CoinGecko API

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Cryptocurrencies = () => {
+
+    useEffect(() => {
+        axios.get('https://api.coingecko.com/api/v3/coins/list')
+            .then(res => console.log(res))
+            .catch(error => console.log(error))
+    })
+    
     return (
         <div className='Cryptocurrencies'>
             Here is a list of cryptocurrencies
