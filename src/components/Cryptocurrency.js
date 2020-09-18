54fc0ee3fd94f8e013eec16318f4607acc55eea2
Ziad <ziad1991@hotmail.co.uk>
2020-09-18 17:33:38 +0100

    Styling tweaks

--- src/components/Cryptocurrency.js
+++ src/components/Cryptocurrency.js
@@ -9,9 +9,9 @@ const Cryptocurrency = ({coins}) => {
                     <p className='name' key={coin.id}> {coin.name} ({(coin.symbol).toUpperCase()}) </p>
                     <img className='image' src={coin.image} alt={coin.name}/>
                     <div className="information">
-                        <p className="price">Current price: ${coin.current_price}</p>
-                        <p className="24hlow">24 hour low: ${coin.low_24h}</p>
-                        <p className="24hhigh">24 hour high: ${coin.high_24h}</p>
+                        <p className="current-price">Current price: ${coin.current_price}</p>
+                        <p className="24hlow">24 Hour Low: ${coin.low_24h}</p>
+                        <p className="24hhigh">24 Hour High: ${coin.high_24h}</p>
                     </div>
                 </div>
             )}
