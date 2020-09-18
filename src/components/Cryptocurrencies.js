53b3e0e357b63dd9a26d4991d4e2ea3b21419eca
Ziad <ziad1991@hotmail.co.uk>
2020-09-18 13:50:19 +0100

    Add main Cryptocurrencies component

---
+++ src/components/Cryptocurrencies.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+    return (
+        <div className='Cryptocurrencies'>
+            Here is a list of cryptocurrencies
+        </div>
+    )
+}
+
+export default Cryptocurrencies
