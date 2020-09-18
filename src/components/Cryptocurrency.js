56cbcedd16d20e1cf485bdd48e6b6ab78194a9b3
Ziad <ziad1991@hotmail.co.uk>
2020-09-18 17:22:29 +0100

    Style card icons

--- src/components/Cryptocurrency.js
+++ src/components/Cryptocurrency.js
@@ -6,13 +6,13 @@ const Cryptocurrency = ({coins}) => {
         <div className='Cryptocurrency'>
             {coins.map((coin) => 
                 <div className='coin'>
-                    <div className='name' key={coin.id}> {coin.name} ({(coin.symbol).toUpperCase()}) </div>
+                    <p className='name' key={coin.id}> {coin.name} ({(coin.symbol).toUpperCase()}) </p>
                     <img className='image' src={coin.image} alt={coin.name}/>
-                        <div className="information">
-                            <p className="price">Current price: ${coin.current_price}</p>
-                            <span className="24hlow">24 hour low: ${coin.low_24h}</span>
-                            <span className="24hhigh">24 hour high: ${coin.high_24h}</span>
-                        </div>
+                    <div className="information">
+                        <p className="price">Current price: ${coin.current_price}</p>
+                        <p className="24hlow">24 hour low: ${coin.low_24h}</p>
+                        <p className="24hhigh">24 hour high: ${coin.high_24h}</p>
+                    </div>
                 </div>
             )}
         </div>
